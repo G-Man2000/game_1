c326c1ed1fef74dc368bd6902c74731f6ac90f02
G-Man2000 <45884985+G-Man2000@users.noreply.github.com>
2020-03-17 00:44:46 +0300

    Add files via upload

---
+++ Assets/script/player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{   
+    private Rigidbody2D rb;
+    public float speed;
+    public Animation anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        rb.transform.Translate(1*(speed/100), 0, 0);
+    }
+
+    public void Jump()
+    {
+
+    }
+}
